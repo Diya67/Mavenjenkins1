1845fc8acd789f8e5202dd5b5663fbded5d09d74
DIYA67 <diyapatelhm67@gmail.com>
2025-06-05 06:29:35 +0000

    Initial commit

---
+++ src/main/java/com/example/AppTest.java
@@ -0,0 +1,15 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Diya -1BI22CS052" )
+
+    }
+}
+
